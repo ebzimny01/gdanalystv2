9bee6366cd7bd4207c3cc0178e6428c674581dff
Ed Zimny <ebzimny@gmail.com>
2020-10-12 12:48:38 -0400

    splitting prod and dev env config for Heroku

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gd.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gd.settings.base')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
