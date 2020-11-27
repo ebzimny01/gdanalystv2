030c0d6a13b429ab1abced5824098cf362ad9ad1
Ed Zimny <ebzimny@gmail.com>
2020-11-27 17:17:24 -0500

    added Sentry integration

--- gd/urls.py
+++ gd/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     path('gdapp_admin/', admin.site.urls),
     path('', include('gdanalyst.urls')),
     path('django-rq/', include('django_rq.urls'))
