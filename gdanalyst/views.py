01d01f67719202a02d36d94c84c4501156f0d04e
Ed Zimny <ebzimny@gmail.com>
2020-12-09 17:02:15 -0500

    Update views.py
    Added latitude and longitude for each school to the table but hidden.
    This data is then rendered through the template views.

--- gdanalyst/views.py
+++ gdanalyst/views.py
@@ -46,7 +46,7 @@ def wisid(request, wisid):
     teamdistance = get_distance(wid.location, hct)
     combined = {}
     for each in hct:
-        combined[each.wis_id] = [each.school_short, each.coach, teamdistance[each.wis_id]]
+        combined[each.wis_id] = [each.school_short, each.coach, teamdistance[each.wis_id], each.location.latitude, each.location.longitude]
     K = 2
     combined_sorted = sorted(combined.items(), key=lambda x: x[1][K])
     return render(request, "gdanalyst/wisid.html", {
@@ -113,7 +113,7 @@ def player(request, worldname, division):
         else:
             combined = {}
             for each in hct:
-                combined[each.wis_id] = [each.school_short, each.coach, player_school_distance[0][each.wis_id]]
+                combined[each.wis_id] = [each.school_short, each.coach, player_school_distance[0][each.wis_id], each.location.latitude, each.location.longitude]
             K = 2
             combined_sorted = sorted(combined.items(), key=lambda x: x[1][K])
             return render(request, "gdanalyst/player.html", {
@@ -140,7 +140,7 @@ def town(request, worldname, division):
     else:
         combined = {}
         for each in hct:
-            combined[each.wis_id] = [each.school_short, each.coach, town_school_distance[0][each.wis_id]]
+            combined[each.wis_id] = [each.school_short, each.coach, town_school_distance[0][each.wis_id], each.location.latitude, each.location.longitude]
         K = 2
         combined_sorted = sorted(combined.items(), key=lambda x: x[1][K])
         return render(request, "gdanalyst/town.html", {
