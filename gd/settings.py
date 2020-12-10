d1902c4a99d23417af5152f798112078b8a644bf
Ed Zimny <ebzimny@gmail.com>
2020-12-09 19:35:29 -0500

    Update settings.py
    heroku complained about importing sec.py

--- gd/settings.py
+++ gd/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from gd.sec import *
+from .sec import *
 import django_heroku
 import os
 import logging
