7512e4091c086b10bc4080d04343b56944756a9b
ebzimny01 <ebzimny@gmail.com>
2023-01-21 16:51:05 -0500

    Update settings.py

--- gd/settings.py
+++ gd/settings.py
@@ -200,7 +200,7 @@ DATABASES = {
 DATABASES = {
     'default': dj_database_url.config()
 }
-DATABASES['default']['OPTIONS'] = {'ssl': {'ca': os.path.join(os.path.dirname(__file__), 'config', 'amazon-rds-ca-cert.pem'}}
+DATABASES['default']['OPTIONS'] = {'ssl': {'ca': os.path.join(os.path.dirname(__file__), 'config', 'amazon-rds-ca-cert.pem')}}
 
 
 # Password validation
