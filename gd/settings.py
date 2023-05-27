44e4744219c98b08a50eb90f719a4d587b5cb36b
Ed Zimny <ebzimny@gmail.com>
2023-05-27 11:57:45 -0400

    vercel changes

--- gd/settings.py
+++ gd/settings.py
@@ -260,7 +260,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
@@ -274,4 +274,4 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
