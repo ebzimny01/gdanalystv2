e9fdd325eea1e9c45684cce851c333d19abc0a1b
Ed Zimny <ebzimny@gmail.com>
2023-05-27 20:37:55 -0400

    build vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,24 @@
 # build_files.sh
-pip install -r requirements.txt
+echo "Installing dependencies..."
+
+python3.9 -m pip install -r requirements.txt
 
 # make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+echo "Migrating database..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Creating superuser..."
+
+DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL}
+DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME}
+DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD}
+
+python3.9 manage.py createsuperuser \
+    --email $DJANGO_SUPERUSER_EMAIL \
+    --username $DJANGO_SUPERUSER_USERNAME \
+    --noinput || true
+
+echo "Collecting static files..."
+
+python3.9 manage.py collectstatic --noinput
