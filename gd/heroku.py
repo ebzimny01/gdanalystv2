e000a3de434a77c3cbcb2549b1d939d5b899b77d
Ed Zimny <ebzimny@gmail.com>
2024-08-06 23:27:08 -0400

    tesing heroku.py redis cache config change

--- gd/heroku.py
+++ gd/heroku.py
@@ -59,12 +59,15 @@ print(redis_url.password)
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://%s:%s' % (redis_url.hostname, redis_url.port), 
+        'LOCATION': 'rediss://%s:%s' % (redis_url.hostname, redis_url.port), 
         'OPTIONS': { 
             'DB': 0,
             'PASSWORD': redis_url.password,
             'CLIENT_CLASS': 'django_redis.client.DefaultClient', 
-            'MAX_ENTRIES': 5000, 
+            'MAX_ENTRIES': 5000,
+            "CONNECTION_POOL_KWARGS": {
+                "ssl_cert_reqs": None  # Disable SSL verification
+            }, 
         }, 
     },
 }
