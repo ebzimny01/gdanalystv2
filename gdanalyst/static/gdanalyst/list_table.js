331a1e007f240ed5353a8acf515d12b5c51fd127
Ed Zimny <ebzimny@gmail.com>
2024-08-18 18:40:47 -0400

    using list.js

---
+++ gdanalyst/static/gdanalyst/list_table.js
@@ -0,0 +1,39 @@
+var options = {
+    valueNames: [ 
+        "G",
+        "T",
+        "Qtr",
+        "Clk",
+        "FP",
+        "Dn",
+        "Dst",
+        "OF",
+        "DF",
+        "DT",
+        "Blz?",
+        "OT",
+        "RD",
+        "Prs",
+        "PD",
+        "Cvrg",
+        "Cvr",
+        "PR",
+        "Px",
+        "Sck",
+        "Pen",
+        "TO",
+        "TD",
+        "YG",
+        "OPM",
+        "DPM",
+        "PBP",
+        "YPEN",
+        "Prg",
+        "Prgx",
+        "H",
+        "A",
+        "Scr"
+     ]
+  };
+  
+  var userList = new List('gameresults', options);
